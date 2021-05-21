7e8245daf7e451bae8a67e67703d907ef6153591
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-21 19:47:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ class Cat {
     this.sex = sex;
   }
 }
- 
+
 class Dog {
   constructor(name, sex) {
     this.name = name;
     this.sex = sex;
   }
 }
- 
+
 class Bird {
   constructor(name, sex) {
     this.name = name;
